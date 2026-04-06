d76a894e1ceff9db78bee40b2f3ee34114a11e10
AdwoaOwusu <owusu34a@mtholyoke.edu>
2026-04-05 20:00:33 -0400

    Set up remote control for car

--- Delivery_Robot.ino
+++ Delivery_Robot.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+#include<IRremote.hpp>
 
+#define IR_RECEIVE_PIN 8
+#define RemotePin 8
+
+
+IRrecv irrecv(RemotePin);
+
+void setup(){
+  setup_main();
+  setup_remote();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  loop_main();
+  loop_remote();
+}
 
+void setup_main() {
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+}
+
+void loop_main() {
+  if(IrReceiver.decode())
+  {
+    delay(200);
+    irrecv.resume();
+  }
 }
--- Remote_Control.ino
+++ Remote_Control.ino
@@ -1,1 +1,91 @@
+#define back_right 3
+#define back_left 6
+#define forward_right 5
+#define forward_left 9
 
+void setup_remote() {
+  Serial.begin(9600);
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+  pinMode(back_right, OUTPUT);
+  pinMode(forward_right, OUTPUT);
+  pinMode(back_left, OUTPUT);
+  pinMode(forward_left, OUTPUT);
+    
+}
+
+void loop_remote() {
+  
+    if(IrReceiver.decode())
+      {
+         int results = IrReceiver.decodedIRData.command;
+         Serial.println(results);
+
+        if (results == 12)//Press FORWARD Button
+        { 
+          Forward();
+        }
+        else if (results == 24)//Press BACK Button
+        { 
+          Backward();
+        }
+         else if (results == 94)//Press LEFT Button
+        { 
+          Left();
+        }
+        else if (results == 8)//Press RIGHT Button
+        { 
+          Right();
+        }
+        else if (results  == 22)//Stop
+        { 
+          Stop();
+        }
+      delay(200);
+      IrReceiver.resume();
+    }
+   }
+   
+ 
+void Backward()
+  {
+  digitalWrite(back_right,HIGH);
+  digitalWrite(forward_right,LOW);
+  digitalWrite(back_left,HIGH);
+  digitalWrite(forward_left,LOW);
+  }
+
+  
+ void Forward()
+  {
+  digitalWrite(back_right,LOW);
+  digitalWrite(forward_right,HIGH);
+  digitalWrite(back_left,LOW);
+  digitalWrite(forward_left,HIGH);
+  }
+
+  
+ void Stop()
+  {
+  digitalWrite(back_right,LOW);
+  digitalWrite(forward_right,LOW);
+  digitalWrite(back_left,LOW);
+  digitalWrite(forward_left,LOW);
+  }
+
+  
+  int Left()
+    {
+    digitalWrite(back_right,LOW);
+    digitalWrite(forward_right,LOW);
+    digitalWrite(back_left,LOW);
+    digitalWrite(forward_left,HIGH);
+    }
+
+    
+  int Right()
+    {
+    digitalWrite(back_right,LOW);
+    digitalWrite(forward_right,HIGH);
+    digitalWrite(back_left,LOW);
+    digitalWrite(forward_left,LOW);
+    }
